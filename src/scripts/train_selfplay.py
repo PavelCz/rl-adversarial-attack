486ab63ac076a0d6553cda2a727a5e2f8c70a2a7
Pavel C <pavelc.git@posteo.net>
2020-12-13 16:06:16 +0100

    Add evaluate function to train_selfplay

--- src/scripts/train_selfplay.py
+++ src/scripts/train_selfplay.py
@@ -6,27 +6,47 @@ from stable_baselines3 import PPO
 
 from src.selfplay.opponent_wrapper import OpponentWrapper
 
-# Initialize environment
-env = gym.make('PongDuel-v0')
-env = OpponentWrapper(env)
 
-main_model = PPO('MlpPolicy', env, verbose=1)
-#model.learn(total_timesteps=10000)
+def main():
+    # Initialize environment
+    env = gym.make('PongDuel-v0')
+    env = OpponentWrapper(env)
 
-secondary_model = copy.deepcopy(main_model)
+    main_model = PPO('MlpPolicy', env, verbose=1)
+    # model.learn(total_timesteps=10000)
 
-env.set_opponent(secondary_model)
+    secondary_model = copy.deepcopy(main_model)
 
-main_model.set_env(env)
+    env.set_opponent(secondary_model)
 
-ep_reward = 0
-# Evaluate the agent
-done = False
-obs = env.reset()
-while not done:
-    action, _states = main_model.predict(obs, deterministic=True)
-    obs, reward, done, info = env.step(action)
-    ep_reward += reward
-    env.render()
-env.close()
-print(ep_reward)
+    # main_model.set_env(env)
+
+    avg_round_reward = evaluate(main_model, env)
+    print(f"Average round reward: {avg_round_reward}")
+
+
+def evaluate(main_model, env):
+    num_eps = 100
+    total_reward = 0
+    total_rounds = 0
+    for episode in range(num_eps):
+        ep_reward = 0
+        # Evaluate the agent
+        done = False
+        obs = env.reset()
+        info = None
+        while not done:
+            action, _states = main_model.predict(obs, deterministic=True)
+            obs, reward, done, info = env.step(action)
+            ep_reward += reward
+            # env.render()
+        total_reward += ep_reward
+        total_rounds += info['rounds']
+    env.close()
+
+    avg_round_reward = total_reward / total_rounds
+    return avg_round_reward
+
+
+if __name__ == '__main__':
+    main()
