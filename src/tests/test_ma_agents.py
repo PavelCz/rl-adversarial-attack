3109aeb05629e843b1f2f14fc9d3cf35ffc3fdaf
Pavel C <pavelc.git@posteo.net>
2021-01-10 15:16:49 +0100

    Add test for flipping function

---
+++ src/tests/test_ma_agents.py
@@ -0,0 +1,14 @@
+import random
+from copy import copy
+
+from src.selfplay.opponent_wrapper import flip_observation_horizontally
+
+
+def test_flip_obs_horiz():
+    obs = [random.random() for _ in range(4)] + [random.randint(0,1) for _ in range(6)]
+    orig_obs = copy(obs)
+    flipped = flip_observation_horizontally(obs)
+
+    assert orig_obs == obs
+    assert flipped[0] == orig_obs[0]
+    assert flipped[2] == orig_obs[2]
