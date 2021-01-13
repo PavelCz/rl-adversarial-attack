cbccb292a990607551ba30eb2d1d05b8c177da2f
Pavel C <pavelc.git@posteo.net>
2021-01-13 19:42:47 +0100

    Improve train function

--- src/rule_based_opponent/train_pong_rule_based.py
+++ src/rule_based_opponent/train_pong_rule_based.py
@@ -18,16 +18,21 @@ def learn(max_agents, num_learn_steps, num_eval_eps, num_skip_steps=0):
     env = gym.make('Pong-v0')
 
     main_model = A2C('CnnPolicy', env, verbose=0, tensorboard_log="output/tb-log")  # , exploration_fraction=0.3)
+
+    print("Learning...")
     main_model.learn(total_timesteps=num_learn_steps, tb_log_name="log")
 
     obs = env.reset()
-
-    while True:
-        action, _states = main_model.predict(obs)
-        obs, rewards, dones, info = env.step(action)
+    rewards = []
+    done = False
+    while not done:
+        action, _states = main_model.predict(obs, deterministic=False)
+        print(action)
+        ob, reward, done, info = env.step(action)
         env.render()
-        print(rewards)
+        rewards.append(reward)
+    print(f"{sum(rewards) / len(rewards)}")
 
 
 if __name__ == '__main__':
-    learn(0, 2_000, 0)
+    learn(0, 2000, 0)
