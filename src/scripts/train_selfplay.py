34054e7468539875e8fa2fb505131aa613f55c2a
Pavel C <pavelc.git@posteo.net>
2021-01-22 01:22:36 +0100

    Improve run file

--- src/scripts/train_selfplay.py
+++ src/scripts/train_selfplay.py
@@ -3,18 +3,18 @@ from src.selfplay.naive_selfplay_training import learn_with_selfplay
 
 def main():
     max_agents = 2
-    num_eval_eps = 1000
-    num_skip_steps_list = [0, 2]
+    num_eval_eps = 100
+    num_skip_steps_list = [0]
     model_type = 'dqn'
-    num_learn_steps_list = [2_000, 20_000, 300_000, 100_000, 750_000, 1_500_000]
+    num_learn_steps_list = [750_000]
 
-    for learn_steps in num_learn_steps_list:
+    for num_learn_steps in num_learn_steps_list:
         for num_skip_steps in num_skip_steps_list:
-            model_name = model_type + '-' + ('' if num_skip_steps == 0 else 'skip-') + str(num_skip_steps / 1000) + 'k-'
+            model_name = model_type + '-' + ('' if num_skip_steps == 0 else 'skip-') + str(num_learn_steps / 1000) + 'k-'
 
             print(f'Running training for model {model_name}')
             learn_with_selfplay(max_agents=max_agents,
-                                num_learn_steps=learn_steps,
+                                num_learn_steps=num_learn_steps,
                                 num_eval_eps=num_eval_eps,
                                 num_skip_steps=num_skip_steps,
                                 model_name=model_name)
