64a4d5e4dd37a6b7c6b6f6127709806c82ec2f36
Pavel C <pavelc.git@posteo.net>
2021-03-03 17:26:17 +0100

    Implement white-box adversarial attack that copies half the actions

--- src/attacks/white_box_adversarial_policy.py
+++ src/attacks/white_box_adversarial_policy.py
@@ -1,9 +1,10 @@
 import gym
 import torch
 import numpy as np
+import time
 
 from src.common.environment_dynamics import approximate_step_pong_duel
-from src.common.observation_utils import flip_observation_horizontally
+from src.common.observation_utils import flip_observation_horizontally, is_ball_moving_towards_player
 
 
 class WhiteBoxAdversarialAgent:
@@ -13,12 +14,71 @@ class WhiteBoxAdversarialAgent:
         self.victim_type = victim_type
 
     def predict(self, obs, deterministic=True):
-        # Choose best action by determining which will cause a worse state for the victim
+        if is_ball_moving_towards_player(obs, 'p1'):
+            # Try to perform good actions by copying victim
+            current_best_action = self._predict_copy_victim(obs)
+        else:
+            # Ball is not moving twoards attacker -> confuse victim by moving adversarially
+            current_best_action = self._predict_adversarially(obs)
+        return current_best_action, None
 
+    def _predict_adversarially(self, obs):
+        # Choose best action by determining which will cause a worse state for the victim
         # Get observation as it should be passed to the victim q_net
         victim_obs = self._switch_obs(obs.copy())
 
-        # Get the victim q_net
+        # Get the logits when performing forward pass with given observations
+        logits = self._victim_q_net_forward(victim_obs)
+
+        victim_action = torch.argmax(logits).item()
+        lowest_max = 1
+        current_best_action = 0
+        for action in range(self.env.action_space.n):
+            # Approximate the next state when victim follows its policy and attacker performs selected action
+            approx_state = approximate_step_pong_duel(self.env, obs.copy(), action_p1=action, action_p2=victim_action)
+
+            # Convert the state to use by victim q_net
+            victim_approx_state = self._switch_obs(approx_state)
+
+            # Get the action the victim will perform in the current state
+            # Get the logits victim q_net in predicted state
+            logits = self._victim_q_net_forward(victim_approx_state)
+
+            # Turn the logits into probability distribution
+            pred = torch.softmax(logits, dim=0)
+            max = torch.max(pred).item()
+            # We are looking for the action that causes the highest entropy
+            # Because pred is a probability distribution that sums up to 1, the action that causes the lowest max value should have the
+            # highest entropy
+            if max < lowest_max:
+                current_best_action = action
+                lowest_max = max
+        # Perform the action with the lowest entropy in the real world
+        return current_best_action
+
+    def _predict_copy_victim(self, obs):
+        # Check the q_net, what would the victim do in this state
+        logits = self._victim_q_net_forward(obs)
+        action = torch.argmax(logits).item()
+        return action
+
+    def _victim_q_net_forward(self, victim_obs: np.ndarray) -> torch.Tensor:
+        """
+        Access the reference to the victim q-net and get the ouput of a single forward pass with the given observations.
+        :param victim_obs: The observation as an numpy array
+        :return: The logits after forward pass as a torch tensor
+        """
+        with torch.no_grad():
+            victim_q_net = self._get_victim_q_net()
+            # Get the action the victim will perform in the current state
+            victim_obs = torch.tensor([victim_obs])
+            # Move obs to correct device
+            victim_obs = victim_obs.to(victim_q_net.device)
+            logits = victim_q_net(victim_obs)
+            logits = logits.squeeze()
+        return logits
+
+    def _get_victim_q_net(self):
         if self.victim_type == 'sb3':
             # Get the victim q_net from a stable-baselines3 model
             victim_q_net = self.victim.q_net
@@ -26,77 +86,7 @@ class WhiteBoxAdversarialAgent:
             raise NotImplementedError('White-box adversarial policy not implemented for NFSP')
         else:
             raise NotImplementedError(f'White-box adversarial policy not implemented for the type {self.victim_type}')
-
-        # Get the action the victim will perform in the current state
-        victim_obs = torch.tensor([victim_obs])
-        # Move obs to correct device
-        victim_obs = victim_obs.to(victim_q_net.device)
-        logits = victim_q_net(victim_obs)
-        victim_action = torch.argmax(logits).item()
-
-        lowest_max = 1
-        current_best_action = 0
-        for action in range(self.env.action_space.n):
-            # Apporximate the next state when victim follows its policy and attacker performs selected action
-            approx_state = approximate_step_pong_duel(self.env, obs.copy(), action_p1=action, action_p2=victim_action)
-
-            # Convert the state to use by victim q_net
-            victim_approx_state = self._switch_obs(approx_state)
-            # Get the action the victim will perform in the current state
-            victim_approx_state = torch.tensor([victim_approx_state])
-            # Move obs to correct device
-            victim_approx_state = victim_approx_state.to(victim_q_net.device)
-
-            # Get the logits victim q_net in predicted state
-            logits = victim_q_net(victim_approx_state)
-            logits = logits.squeeze()
-            # Turn the logits into probability distribution
-            pred = torch.softmax(logits, dim=0)
-            max = torch.max(pred).item()
-            # We are looking for the action that causes the highest entropy
-            # Because pred is a probability distribution that sums up to 1, the action that causes the lowest max value should have the
-            # highest entropy
-            if max < lowest_max:
-                current_best_action = action
-                lowest_max = max
-        # Perform the action with the lowest entropy in the real world
-        return current_best_action, None
-
-    def predict_slow(self, obs, deterministic=True):
-        # Choose best action by determining which will cause a worse state for the victim
-        # obs not necessary here, as the state is implicitly in the current state of self.env
-
-        # Make copy of environment for internal lookahead
-        sim_env = copy.deepcopy(self.env)
-
-        victim_q_net = self.victim.q_net
-
-        lowest_max = 1
-        current_best_action = 0
-        for action in range(self.env.action_space.n):
-            next_obs, _, _, _ = sim_env.step(action)
-
-            # This is the new observation for us, this gets us the next observation for our victim
-            # sim_env.w
-
-            next_obs = torch.tensor([next_obs])
-            # Move obs to correct device
-            next_obs = next_obs.to(victim_q_net.device)
-
-            # Get the logits victim q_net in predicted state
-            logits = victim_q_net(next_obs)
-            logits = logits.squeeze()
-            # Turn the logits into probability distribution
-            pred = torch.softmax(logits, dim=0)
-            max = torch.max(pred).item()
-            # We are looking for the action that causes the highest entropy
-            # Because pred is a probability distribution that sums up to 1, the action that causes the lowest max value should have the
-            # highest entropy
-            if max < lowest_max:
-                current_best_action = action
-                lowest_max = max
-        # Perform the action with the lowest entropy in the real world
-        return current_best_action, None
+        return victim_q_net
 
     def _switch_obs(self, obs):
         """
