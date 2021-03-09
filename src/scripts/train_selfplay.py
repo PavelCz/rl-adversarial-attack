6962914560faf99b67cd8a26a89a4e2f6c33b3ac
Pavel C <pavelc.git@posteo.net>
2021-03-09 17:44:21 +0100

    Change training script

--- src/scripts/train_selfplay.py
+++ src/scripts/train_selfplay.py
@@ -3,15 +3,16 @@ from src.selfplay.naive_selfplay_training import learn_with_selfplay
 
 def main():
     # Settings for training and evaluation
-    max_agents = 8
+    max_agents = 9
     num_eval_eps = 50
     num_learn_steps = 1_000_000
     num_learn_steps_pre_training = 1_000_000  # pre-training is done only against rule-based opponent
     patience = 20
     image_observations = False
     output_folder = "output"
+    adversarial_training = None
 
-    model_name = 'adversarial-train'
+    model_name = 'modelA+'
 
     print(f'Running training for model {model_name}')
     learn_with_selfplay(max_agents=max_agents,
@@ -22,7 +23,7 @@ def main():
                         patience=patience,
                         image_observations=image_observations,
                         output_folder=output_folder,
-                        adversarial_training=0.5)
+                        adversarial_training=adversarial_training)
 
 
 if __name__ == '__main__':
