5e75bf6622d425367af622758d1fd70e6a635821
Pavel C <pavelc.git@posteo.net>
2021-03-09 09:44:48 +0100

    Update selfplay script

--- src/scripts/train_selfplay.py
+++ src/scripts/train_selfplay.py
@@ -3,15 +3,15 @@ from src.selfplay.naive_selfplay_training import learn_with_selfplay
 
 def main():
     # Settings for training and evaluation
-    max_agents = 20
+    max_agents = 8
     num_eval_eps = 50
-    num_learn_steps = 200_000
-    num_learn_steps_pre_training = 200_000  # pre-training is done only against rule-based opponent
-    patience = 15
+    num_learn_steps = 1_000_000
+    num_learn_steps_pre_training = 1_000_000  # pre-training is done only against rule-based opponent
+    patience = 20
     image_observations = False
     output_folder = "output"
 
-    model_name = 'adversarial-train-feature-obs-fewer-steps'
+    model_name = 'adversarial-train-feature-obs'
 
     print(f'Running training for model {model_name}')
     learn_with_selfplay(max_agents=max_agents,
@@ -22,7 +22,7 @@ def main():
                         patience=patience,
                         image_observations=image_observations,
                         output_folder=output_folder,
-                        adversarial_training=None)
+                        adversarial_training=0.5)
 
 
 if __name__ == '__main__':
